67153fda536031a05368ddd6c60f8f95eee5822f
spdev <spdevuk@gmail.com>
2018-05-03 00:39:40 +0100

    ability to make a store and save it to mongoDB

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,7 @@
+const mongoose = require ('mongoose');
+const Store = mongoose.model('Store');
+
+
 exports.homePage = (req, res) => {
   res.render('index');
 }
@@ -6,6 +10,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 }
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
-}
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
