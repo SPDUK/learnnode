83d337351412fec4edf43781a80515606d91bbf8
spdev <spdevuk@gmail.com>
2018-05-03 04:14:34 +0100

    added google autocomplete on location form & prevented enter key from entering form

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,6 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+autocomplete(document.getElementById('address'), document.getElementById('lat'), document.getElementById('lng'));
+
