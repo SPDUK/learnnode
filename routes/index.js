3b68c829976a654ec6a14817f9a2cc0506c8c3a3
Steve <spdevuk@gmail.com>
2018-05-03 11:55:16 +0100

    added tags page & tag styling + active tag styling

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,7 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 // view for a storeController
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
 module.exports = router;
