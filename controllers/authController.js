5107312f947c772dadf930817e9cbb1d9aa2210f
Steve <spdevuk@gmail.com>
2018-05-04 07:10:17 +0100

    added password reset confirm  page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -21,4 +24,45 @@ exports.isLoggedIn = (req, res, next) => {
   } 
     req.flash('error', 'You must be logged in to do that');
     res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+  // see if the user with that email exists 
+  const user = await User.findOne({ email: req.body.email });
+  if(!user) {
+    // pretend to send a reset even if the email does not exist
+    req.flash('error', 'A password reset has been mailed to you.');
+    return res.redirect('/login');
+  }
+  // reset tokens and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 360000;  // 1 hour from now
+  await user.save();
+  // send them an email with the token
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
+  // redirect to the login page 
+  res.redirect('/login');
+};
+
+exports.reset = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+  if (!user) {
+    req.flash('error', 'Password reset token is invalid or has expired');
+    return res.redirect('/login');
+  }
+  // if there is a user show a reset password form
+  res.render('reset', { title: 'Reset your Password' });
+};
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']) {
+    next();
+    return; 
+  }
+  req.flash('error', 'Passwords do not match');
+  res.redirect('back');
 };
--- routes/index.js
+++ routes/index.js
@@ -50,5 +50,12 @@ router.get('/logout', authController.logout);
 // user account
 router.get('/account', authController.isLoggedIn, userController.account);
 router.post('/account', catchErrors(userController.updateAccount))
+// account recovery
+router.post('/account/forgot', catchErrors(authController.forgot));
+router.get('/account/reset/:token', catchErrors(authController.reset));
+router.post('/account/reset/:token',
+ authController.confirmedPasswords,
+ catchErrors(authController.update)
+);
 
 module.exports = router;
